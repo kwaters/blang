f41dc686bf6b6d0869b3f8ff5ca12a0b5ba7469e
Kenneth Waters <kwwaters@gmail.com>
2017-02-15 23:03:22 -0800

    Add complex string formatting.
    - Add a formatting mini-language to simplify pretty-printers.

--- b/Build.mk
+++ b/Build.mk
@@ -11,6 +11,7 @@ blang1 += $(D)irgen.o
 blang1 += $(D)lexer.o
 blang1 += $(D)lrvalue.o
 blang1 += $(D)nametabl.o
+blang1 += $(D)obuffer.o
 blang1 += $(D)parser.o
 blang1 += $(D)string.o
 blang1 += $(D)vector.o
